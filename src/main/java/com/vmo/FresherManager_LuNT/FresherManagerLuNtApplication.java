e6e20642ad76f90f5af9ceee0b625e962e7d8bcd
lunguyenhe <lunthe150983@fpt.edu.vn>
2022-11-07 09:34:54 +0700

    initial commit

---
+++ src/main/java/com/vmo/FresherManager_LuNT/FresherManagerLuNtApplication.java
@@ -0,0 +1,13 @@
+package com.vmo.FresherManager_LuNT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FresherManagerLuNtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FresherManagerLuNtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmo/FresherManager_LuNT/FresherManagerLuNtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmo.FresherManager_LuNT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FresherManagerLuNtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
